bc2642c9260d71509c4e17432d00ff7331fcebb0
mrojas <kdashcr@gmail.com>
2020-09-11 15:02:32 -0600

    fixing web token evn variable

--- src/models/user.js
+++ src/models/user.js
@@ -85,7 +85,7 @@ userSchema.methods.generateAuthToken = async function () {
     const user = this
     const _id = user._id.toString()
 
-    const token = jwt.sign({ _id }, 'thisismynewcourse')
+    const token = jwt.sign({ _id }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
